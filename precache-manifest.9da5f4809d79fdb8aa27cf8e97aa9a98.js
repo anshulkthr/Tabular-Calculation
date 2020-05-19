64c98430ddc2760f6f40d7d5fb9b5b2372aaaad6
anshul kothari <anshul@anshuls-MacBook-Pro.local>
2020-05-19 15:11:31 +0530

    Updates

---
+++ precache-manifest.9da5f4809d79fdb8aa27cf8e97aa9a98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf5f439e2a9dc403fa2868b151153d3a",
+    "url": "/Tabular-Calculation/index.html"
+  },
+  {
+    "revision": "ba0ed6fdf4eb43b65134",
+    "url": "/Tabular-Calculation/static/css/main.cd6df59e.chunk.css"
+  },
+  {
+    "revision": "8efc7f201b79a9977fd0",
+    "url": "/Tabular-Calculation/static/js/2.884a58e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tabular-Calculation/static/js/2.884a58e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba0ed6fdf4eb43b65134",
+    "url": "/Tabular-Calculation/static/js/main.cebe7781.chunk.js"
+  },
+  {
+    "revision": "e4192bfabec7ebdae397",
+    "url": "/Tabular-Calculation/static/js/runtime-main.268a58aa.js"
+  }
+]);
